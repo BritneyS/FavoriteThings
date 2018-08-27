7c11e196307179ea8fda847f892ed0b582dbf985
Britney Smith <brit.smith7@gmail.com>
2018-08-27 16:35:18 -0400

    Set up TableViewController

---
+++ FavoriteThings/FavoriteThingsTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  FavoriteThings
+//
+//  Created by Britney Smith on 8/27/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+/*
+ Favorite Things
+ 
+ Create an app that displays your favorite things in a
+ table view
+ 
+ • Implement swipe-to-delete functionality in your
+ table view
+ 
+ • You can choose to display your favorite songs,
+ animals, random things, or anything else you want
+ — the goal here is to practice creating a Table View
+ Controller, populate your table view from a data
+ source, and manage your table view’s behavior
+ using delegate methods
+ */
+import UIKit
+
+class FavoriteThingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
